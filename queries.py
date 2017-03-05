49d2f5fb8bddab908893907265cb18437943bb34
Rajendra Kumar yadav <rajendra.yadav96@gmail.com>
2017-03-05 22:52:02 +0530

    Update queries.py

--- queries.py
+++ queries.py
@@ -1,22 +1,6 @@
 class Queries (object):
     get_last_inserted_id = "select last_insert_id() as id"
 
-    insert_sample_data_info = "insert into excel_sheet_data (excel_id ,databasecategory " \
-                              " ,businessname ,address1, address2, locality, town, " \
-                              " country ,postcode, area, postcodearea ,postcodesubarea " \
-                              " ,lineofbusiness , sicnumeric , sicdiscription ,telephone" \
-                              "  ,tps , fax ,fps ,employeesnumeric, premisestype ,title1" \
-                              "  ,fname ,sname ,position1 ,email , emailaddress) " \
-                              " values (:excel_id ,:databasecategory , :businessname " \
-                              " ,:address1, :address2, :locality, :town, :country ," \
-                              "  :postcode, :area, :postcodearea , :postcodesubarea " \
-                              " , :lineofbusiness , :sicnumeric , :sicdiscription " \
-                              " ,:telephone ,:tps , :fax ,:fps ,:employeesnumeric, " \
-                              " :premisestype ,:title1 ,:fname ,:sname ,:position1 ," \
-                              " :email , :emailaddress) "
-
-    get_excel_sheet_info = "select id, excel_id, databasecategory, businessname, address1, address2, locality, town, country, postcode, area, postcodearea, postcodesubarea, lineofbusiness, sicnumeric, sicdiscription, telephone, tps, fax, fps, employeesnumeric, premisestype, title1, fname, sname, position1, email, emailaddress from excel_sheet_data limit :startlimit, :count"
-
     checking_for_parking_space_availability = "select slotno from parkingslot where status = :status and slotno > 0"
 
     get_parking_slot_id_using_slot_no = "select id from parkingslot where slotno = :slotno"
